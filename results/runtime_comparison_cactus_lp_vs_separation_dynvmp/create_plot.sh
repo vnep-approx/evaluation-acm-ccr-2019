61aabf5bb5f2cb11555d63b612226782e989322d
Matthias Rost <mrost@inet.tu-berlin.de>
2019-05-16 19:41:23 +0200

    preparing upload of final results

--- results/runtime_comparison_cactus_lp_vs_separation_dynvmp/create_plot.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python -m evaluation_acm_ccr_2019.cli evaluate_results ifip_networking_separation_lp_solutions_after_refactor_reduced.pickle ifip_networking_evaluation_solutions_rand_round_reduced.pickle ./output/ --exclude_generation_parameters "{'number_of_requests': [20]}"
--- results/treewidth_computation_evaluation/run_experiments.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python -m evaluation_acm_ccr_2019.cli execute_treewidth_computation_experiment --threads 2 treewidth_computation_random_graphs_5_50_0.01_0.5_1000.yml
